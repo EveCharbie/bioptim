1d111e2b4ad0c4aa6eaa539615a66748138b20f4
EveCharbie <eve.charbonneau.1@umontreal.ca>
2021-12-30 15:14:20 -0500

    Stated the problem that I want to make robust (Tkatchev)
    changed the model to match both examples

---
+++ bioptim/examples/torque_driven_ocp/robust_optimal_control.py
@@ -0,0 +1,148 @@
+"""
+Probleme temporaire pour montrer le ROCP sur des mouvements de barre fixe (Tkatchev).
+À ajouter un bon modèle de gymnaste.
+Idéalement pouvoir comparer notre méthode avec celle de https://www.sciencedirect.com/science/article/pii/S0167945711001680?casa_token=76oAyGt3Zs4AAAAA:5Xo1zlcjC8hXwz80JuF7E5vfmx-nUxjKIkXjVixY50nE0LmwU7f3Q5ywiJWU6MbkyVw6qoaJ
+En temre de techniques et de temps de calcul.
+"""
+
+
+import biorbd_casadi as biorbd
+from bioptim import (
+    OptimalControlProgram,
+    DynamicsList,
+    DynamicsFcn,
+    ObjectiveList,
+    ConstraintList,
+    ConstraintFcn,
+    BoundsList,
+    InitialGuessList,
+    Node,
+    QAndQDotBounds,
+    ObjectiveFcn,
+    BiMappingList,
+    Axis,
+    PhaseTransitionList,
+    PhaseTransitionFcn,
+    BiMapping,
+    CostType,
+    Solver,
+)
+
+
+
+def prepare_ocp(
+    biorbd_model_path: str = "models/double_pendulum.bioMod",
+    biorbd_model_path_withTranslations: str = "models/double_pendulum_with_translations.bioMod",
+) -> OptimalControlProgram:
+
+    biorbd_model = (biorbd.Model(biorbd_model_path), biorbd.Model(biorbd_model_path_withTranslations), biorbd.Model(biorbd_model_path_withTranslations))
+
+    # Problem parameters
+    n_shooting = (40, 40, 40)
+    final_time = (1, 1, 1)
+    tau_min, tau_max, tau_init = -200, 200, 0
+
+    # Mapping
+    tau_mappings = BiMappingList()
+    tau_mappings.add("tau", [None, 0], [1], phase=0)
+    tau_mappings.add("tau", [None, None, None, 0], [3], phase=1)
+    tau_mappings.add("tau", [None, None, None, 0], [3], phase=2)
+
+    # Add objective functions
+    objective_functions = ObjectiveList()
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", weight=1, phase=0)
+    objective_functions.add(ObjectiveFcn.Lagrange.MINIMIZE_CONTROL, key="tau", weight=1, phase=1)
+    objective_functions.add(
+        ObjectiveFcn.Mayer.MINIMIZE_COM_POSITION, node=Node.END, weight=-1000, axes=Axis.Z, phase=1, quadratic=False
+    )
+    objective_functions.add(ObjectiveFcn.Mayer.MINIMIZE_COM_POSITION, node=Node.END, weight=-100, axes=Axis.Y, phase=1)
+    # objective_functions.add(ObjectiveFcn.Mayer.SUPERIMPOSE_MARKERS, node=Node.END, first_marker="bar", second_marker="marker_1", phase=2, weight=100000)
+
+    # Constraints
+    constraints = ConstraintList()
+    constraints.add(ConstraintFcn.TIME_CONSTRAINT, node=Node.END, min_bound=0.1, max_bound=2, phase=0)
+    constraints.add(ConstraintFcn.TIME_CONSTRAINT, node=Node.END, min_bound=0.1, max_bound=2, phase=1)
+    constraints.add(ConstraintFcn.TIME_CONSTRAINT, node=Node.END, min_bound=0.1, max_bound=2, phase=2)
+    constraints.add(ConstraintFcn.TRACK_COM_POSITION, node=Node.END, axes=Axis.Z, phase=1, quadratic=False, min_bound=0, max_bound=2)
+    constraints.add(ConstraintFcn.SUPERIMPOSE_MARKERS, node=Node.END, first_marker="bar", second_marker="marker_1", phase=2)
+
+    # Dynamics
+    dynamics = DynamicsList()
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN, with_contact=False)
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN, with_contact=False)
+    dynamics.add(DynamicsFcn.TORQUE_DRIVEN, with_contact=False)
+
+    # Path constraint
+    x_bounds = BoundsList()
+    x_bounds.add(bounds=QAndQDotBounds(biorbd_model[0]))
+    x_bounds.add(bounds=QAndQDotBounds(biorbd_model[1]))
+    x_bounds.add(bounds=QAndQDotBounds(biorbd_model[2]))
+
+    # Phase 0
+    x_bounds[0].min[0, :] = 3.14
+    x_bounds[0][1, 0] = 0
+    x_bounds[0][0, 0] = 3.14
+    x_bounds[0].min[0, -1] = 2 * 3.14
+
+    # Phase 1
+    x_bounds[1].min[2, :] = 3.14
+    x_bounds[1][[0, 1, 4, 5], 0] = 0
+    # x_bounds[1].min[2, -1] = 3.1416
+    # x_bounds[1].max[2, -1] = 2 * 3.1416
+
+    # Phase 2
+    x_bounds[2].min[2, -1] = 5/4 * 3.1416
+    x_bounds[2].max[2, -1] = 2 * 3.1416
+    # x_bounds[2].min[3, -1] = 0
+    # je pourrais ajouter des contraintes pour les mains sur la barre ici a la place au besoin
+
+    # Initial guess
+    x_init = InitialGuessList()
+    x_init.add([0] * (biorbd_model[0].nbQ() + biorbd_model[0].nbQdot()))
+    x_init.add([0] * (biorbd_model[1].nbQ() + biorbd_model[1].nbQdot()))
+    x_init.add([0] * (biorbd_model[2].nbQ() + biorbd_model[2].nbQdot()))
+
+    # Define control path constraint
+    u_bounds = BoundsList()
+    u_bounds.add([tau_min] * len(tau_mappings[0]["tau"].to_first), [tau_max] * len(tau_mappings[0]["tau"].to_first))
+    u_bounds.add([tau_min] * len(tau_mappings[1]["tau"].to_first), [tau_max] * len(tau_mappings[1]["tau"].to_first))
+    u_bounds.add([tau_min] * len(tau_mappings[2]["tau"].to_first), [tau_max] * len(tau_mappings[2]["tau"].to_first))
+
+    # Control initial guess
+    u_init = InitialGuessList()
+    u_init.add([tau_init] * len(tau_mappings[0]["tau"].to_first))
+    u_init.add([tau_init] * len(tau_mappings[1]["tau"].to_first))
+    u_init.add([tau_init] * len(tau_mappings[2]["tau"].to_first))
+
+    phase_transitions = PhaseTransitionList()
+    phase_transitions.add(
+        PhaseTransitionFcn.CONTINUOUS, phase_pre_idx=0, states_mapping=BiMapping([0, 1, 2, 3], [2, 3, 6, 7])
+    )
+
+    return OptimalControlProgram(
+        biorbd_model,
+        dynamics,
+        n_shooting,
+        final_time,
+        x_init=x_init,
+        u_init=u_init,
+        x_bounds=x_bounds,
+        u_bounds=u_bounds,
+        objective_functions=objective_functions,
+        constraints=constraints,
+        variable_mappings=tau_mappings,
+        phase_transitions=phase_transitions,
+    )
+
+
+def main():
+
+    # --- Prepare the ocp --- #
+    ocp = prepare_ocp()
+    ocp.add_plot_penalty(CostType.ALL)
+    sol = ocp.solve(Solver.IPOPT()) # show_online_optim=True
+    sol.animate()
+
+
+if __name__ == "__main__":
+    main()
